09b9f96ced1346446ce536b70fca2518d41ab32b
MyMacbook <daniliandan100500@gmail.com>
2025-11-23 13:13:31 +0100

    Add DesignPattern solution

---
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Factory
+{
+    public static class DatabaseFactory
+    {
+        public static IDatabase CreateDatabase(DatabaseType databaseType)
+        {
+            return databaseType == DatabaseType.SqlServer ? new SqlServerDatabase() : new OracleDatabase();
+        }
+    }
+}
